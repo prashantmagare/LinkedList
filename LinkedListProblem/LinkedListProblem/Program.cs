6a32da2fb042b3ddfaf9661382a9c8774103791e
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 22:55:20 +0530

    "Added - UC1_LinkedList"

--- LinkedListProblem/LinkedListProblem/Program.cs
+++ LinkedListProblem/LinkedListProblem/Program.cs
@@ -7,6 +7,11 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To LinkedList Program ");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
