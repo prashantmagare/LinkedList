c587d4530ad75c3c2a8e2e1d5ad5bfe7bc92eafc
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 23:22:09 +0530

    "Added - UC10_Sorted"

--- LinkedListProblem/LinkedListProblem/Program.cs
+++ LinkedListProblem/LinkedListProblem/Program.cs
@@ -7,12 +7,13 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To LinkedList Program ");
-            LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(40);
-            list.Add(70);
-            list.Display();
+            //LinkedList list = new LinkedList();
+            //list.Add(56);
+            //int a = list.Search(56);
+            //list.InsertAtParticulatPosition(a+1,30);
+            //int b = list.Search(30);
+            //list.InsertAtParticulatPosition(b + 1, 70);
+            //list.Display();
             //Console.WriteLine("Popping first Element in List");
             //list.PoP_First_element();
             //list.Display();
@@ -23,11 +24,17 @@ namespace LinkedList
             //Console.WriteLine("index position of element 30 is :" + a);
             //list.InsertAtParticulatPosition(a + 1, 40);
             //list.Display();
-            Console.WriteLine("Popping element 40 in List");
-            int b = list.Search(40);
-            Console.WriteLine("index position of element 40 is :" + b);
-            list.PoP_Paricular_element(3);
-            list.Display();
+            //Console.WriteLine("Popping element 40 in List");
+            //int b = list.Search(40);
+            //Console.WriteLine("index position of element 40 is :" + b);
+            //list.PoP_Paricular_element(3);
+            //list.Display();
+            SortedLinkedList sl = new SortedLinkedList();
+            sl.Add(56);
+            sl.Add(30);
+            sl.Add(40);
+            sl.Add(70);
+            sl.Display();
         }
     }
 }
